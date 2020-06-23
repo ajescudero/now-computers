47d700ed0d5334537196293b9a5974e8ece2e1b5
Aldo Escudero <ajescudero@gmail.com>
2020-06-23 03:28:43 -0300

    adds devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'stores#index'
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :products
-  resources :stores
+  resources :stores do
+    resources :products
+  end
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ Category.create(name: 'Storage', description: '')
 Category.create(name: 'Electronics', description: '')
 Category.create(name: 'Hardware', description: '')
 
-Product.create(name: 'Samsung MU-PC500T/AM', description: '500GB Portable T7 SSD USB 3.2 Reads up to 1050MB/s Writes up to 1000MB/s Gray', release_date: "06-20-2020", category_id: 1, store_id: 1, price: 60.99)
-Product.create(name: 'WD WDBYFT0040BBK My Passport', description: '4TB 2.5in USB 3.0 Portable External Hard Drive Black', release_date: "06-20-2020", category_id: 1, store_id: 1, price: 82.99)
-Product.create(name: 'Portable Wearable Mini Fans', description: 'Portable Wearable Mini Fans Black', release_date: "06-20-2020", category_id: 2, store_id: 1, price: 30.00)
-Product.create(name: 'REMAX Super PD', description: 'Super PD Fast Charging USB-C To USB-C DataCable RC-151cc Silver', release_date: "06-20-2020", category_id: 2, store_id: 1, price: 32.00)
-Product.create(name: 'Asus ROG Strix Z390-E Gaming', description: 'Asus ROG Strix Z390-E Gaming Intel Z390 LGA 1151Gen8/9 Only DDR4 *4 SLI/CrossFire Ready M.2 *2 Slots', release_date: "06-20-2020", category_id: 3, store_id: 1, price: 249.95)
-Product.create(name: 'Asus TUF GAMING B550M-PLUS (WI-FI) MATX', description: 'Motherboard Socket AM4 AMD B550 Chipset DDR4 4400MHz (Max 128GB) M.2 USB 3.2', release_date: "06-20-2020", category_id: 3, store_id: 1, price: 179.95)
+Product.create(name: 'Samsung MU-PC500T/AM', description: '500GB Portable T7 SSD USB 3.2 Reads up to 1050MB/s Writes up to 1000MB/s Gray', release_date: Time.new, category_id: 1, store_id: 1, price: 60.99)
+Product.create(name: 'WD WDBYFT0040BBK My Passport', description: '4TB 2.5in USB 3.0 Portable External Hard Drive Black', release_date: Time.new, category_id: 1, store_id: 1, price: 82.99)
+Product.create(name: 'Portable Wearable Mini Fans', description: 'Portable Wearable Mini Fans Black', release_date: Time.new, category_id: 2, store_id: 1, price: 30.00)
+Product.create(name: 'REMAX Super PD', description: 'Super PD Fast Charging USB-C To USB-C DataCable RC-151cc Silver', release_date: Time.new, category_id: 2, store_id: 1, price: 32.00)
+Product.create(name: 'Asus ROG Strix Z390-E Gaming', description: 'Asus ROG Strix Z390-E Gaming Intel Z390 LGA 1151Gen8/9 Only DDR4 *4 SLI/CrossFire Ready M.2 *2 Slots', release_date: Time.new, category_id: 3, store_id: 1, price: 249.95)
+Product.create(name: 'Asus TUF GAMING B550M-PLUS (WI-FI) MATX', description: 'Motherboard Socket AM4 AMD B550 Chipset DDR4 4400MHz (Max 128GB) M.2 USB 3.2', release_date: Time.new, category_id: 3, store_id: 1, price: 179.95)
 
 
